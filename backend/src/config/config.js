5ef11e5776500183ad7de8abc24cbd23ad2812f1
Nitesh bakhla <niteshbakhla007@gmail.com>
2026-01-07 04:32:34 +0530

    added configuration

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -5,7 +5,8 @@ const _config = {
             GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
             MONGO_URI: process.env.MONGO_URI,
             JWT_SECRET: process.env.JWT_SECRET,
-            NODE_ENV: process.env.NODE_ENV
+            NODE_ENV: process.env.NODE_ENV,
+            CLIENT_URL:process.env.CLIENT_URL
 }
 
 export default Object.freeze(_config);
